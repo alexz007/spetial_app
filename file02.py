4880b400c9d607832492f835eda6101b8f87e193
azakharov <zakharov.au@gmail.com>
2022-02-24 17:29:56 +0300

    change03

--- file02.py
+++ file02.py
@@ -5,6 +5,5 @@ print('r12345')
 print('выберите ')
 print('выберите пункт доставки кнопка4563')
 
-print('color')
 print('new contact +79851355260')
 
--- file02copy.py
+++ file02copy.py
@@ -1,1 +1,2 @@
 print('test_newrepo')
+print('test_newrepo02')
